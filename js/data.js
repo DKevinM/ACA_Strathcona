d115855f254607e0231b9818bf1d6dd22dd89012
DKevinM <35807537+DKevinM@users.noreply.github.com>
2025-06-15 17:35:24 -0600

    Update data.js

--- js/data.js
+++ js/data.js
@@ -116,9 +116,9 @@ window.fetchRecentStationData = function(stationName) {
 
 
   const rows = orderedParams
-    .filter(p => latestPerParam[p])
-    .map(p => {
-      const r = latestPerParam[p];
+  .filter(p => latestPerParam[p])
+  .map(p => {
+    const r = latestPerParam[p];
       const label = r.Shortform || p;
       const value = r.Value;
       const unit = r.Units || "";
